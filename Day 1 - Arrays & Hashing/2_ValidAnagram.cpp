39b3b5cc7a12fa5af62347396f5b9a6ff82add07
vedangp12 <vedang.paithankar2021@vitstudent.ac.in>
2023-09-08 14:46:45 +0530

    Day 1

---
+++ Day 1 - Arrays & Hashing/2_ValidAnagram.cpp
@@ -0,0 +1,16 @@
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+*/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        if(s==t)
+            return true;
+        return false;
+    }
+};
